59f7b086b65386d17068a34fb3755041072f1b9c
Connor Engel <cengel@lcs.on.ca>
2025-02-21 11:09:47 -0500

    Added the Power Model

---
+++ MVVMAndFreeformInput/Model/Power.swift
@@ -0,0 +1,36 @@
+//
+//  Power.swift
+//  MVVMAndFreeformInput
+//
+//  Created by Connor Engel on 2025-02-21.
+//
+
+import Foundation
+
+struct Power {
+    
+    //MARK: Stored Properties
+    
+    //The base of a power can hold any number
+    var base: Double
+    
+    // The exponent of the power can hold and whole number
+    var exponent: Int
+    
+    //MARK: Computed Properties
+    var result: Double {
+        
+        // Start by making the solution equal to 1
+        var solution = 1.0
+        
+        // Repeatedly multiply the base by itself
+        // as many more times as needed
+        for _ in 1...exponent {
+            solution *= base
+        }
+        
+        // Return the solution
+        return solution
+        
+    }
+}
